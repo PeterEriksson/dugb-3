c2f60d05814ab38fe812149bb826743fa6113375
Peter Eriksson <tuvoc1@hotmail.com>
2021-12-17 15:12:22 +0100

    Update Widgets.js

--- components/Widgets.js
+++ components/Widgets.js
@@ -6,14 +6,21 @@ import {
 
 function Widgets() {
   return (
-    <div className={`hidden mdLgTest:inline-flex flex-col pr-5 //mr-3.5`}>
+    <div
+      className={`hidden //mdLgTest:inline-flex flex-col pr-5 //mr-3.5  mdLgTest:flex //w-full`}
+    >
       <h2 className="mt-3 ml-5 text-2xl font-bold">Senaste</h2>
       <div
         aria-label="widgets__container"
-        className=" mt-4 ml-5 p-5 bg-grayish rounded-widgetsBorder sticky top-0"
+        className=" mt-4 ml-5 p-5 bg-grayish rounded-widgetsBorder sticky top-0    "
       >
-        <h3 className="text-lg font-bold mb-3">Har du kört?</h3>
-
+        <h3 className="text-lg font-bold //mb-3 ">Har du kört?</h3>
+        <p
+          className="opacity-0 px-24 text-xs font-extralight"
+          aria-label="TEMP SOLUTION to increase widgets width"
+        >
+          hejhej
+        </p>
         {/* Documentation: https://saurabhnemade.github.io/react-twitter-embed/?path=/story/twitter-timeline-embed--timeline-profile-with-screen-name */}
         <TwitterTimelineEmbed
           sourceType="profile"
@@ -21,13 +28,14 @@ function Widgets() {
           options={{
             /* height:400, */
             height: 500,
-            width: 200,
+            /* width: 230, */
           }}
           /* borderColor="#e6ecf0" */
           /* (bottom line is meant, not surrounding border) */
-          /* noHeader */
+          noHeader
           /* noFooter */
         />
+
         {/* <TwitterTweetEmbed tweetId={"1356641505889378305"} /> */}
       </div>
     </div>
