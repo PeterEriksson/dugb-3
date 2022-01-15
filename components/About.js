61ad0aa5e570fa15fa043a4ba0f4195b25a89f77
Peter Eriksson <tuvoc1@hotmail.com>
2022-01-15 18:28:26 +0100

    update
    update

--- components/About.js
+++ components/About.js
@@ -12,7 +12,7 @@ function About() {
         din och dina vänners statistik. Välkommen till det heliga gibblocket 3.0
       </p>
       <img
-        className="rounded-2xl "
+        className="rounded-2xl mx-5 mt-2"
         alt=""
         src="https://i.pinimg.com/236x/21/15/0d/21150d9a9397a97c8d796e62214c907b.jpg"
       />
--- pages/loadouts.js
+++ pages/loadouts.js
@@ -1,14 +1,15 @@
 function loadouts() {
   return (
     <div className="w-full  flex flex-col border-l border-r border-grayish">
-      <p className="text-2xl font-bold mx-3 mt-3">Loadouts</p>
-      <p className="font-light mx-3">
-        Oklart vad denna sida ska vara! Kanske ett ställe där swimmerboi listar
+      <p className="text-2xl font-bold mx-3 mt-3 text-center">Loadouts</p>
+
+      <div className="border-b border-grayish" />
+      <p className="font-light mx-3 mt-2">
+        Oklart vad denna sida ska vara! Kanske ett ställe där swimmerboi
         rekommenderar sina bästa loadouts? Posta gärna era förslag i Home
       </p>
-      <div className="border-b border-grayish" />
       <img
-        className="rounded-xl mt-5 "
+        className="rounded-xl mt-3 mx-5"
         alt=""
         src="https://i.pinimg.com/236x/d8/c9/94/d8c9948c7990d9030ff9a786c8adf2a2.jpg"
       />
